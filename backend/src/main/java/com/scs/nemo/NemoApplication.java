5aefedf4bd9d4f503962002f7b2e11fecaf1dd95
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-06-11 19:28:44 +0100

    Update NemoApplication

--- backend/src/main/java/com/scs/nemo/NemoApplication.java
+++ backend/src/main/java/com/scs/nemo/NemoApplication.java
@@ -1,10 +1,17 @@
 package com.scs.nemo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NemoApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NemoApplication.class, args);
