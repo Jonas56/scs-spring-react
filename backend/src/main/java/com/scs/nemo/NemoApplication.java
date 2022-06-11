b95fcf14b624469604c883ee0fe7fb414f513633
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-06-11 02:20:41 +0100

    Initilize spring project. #2

---
+++ backend/src/main/java/com/scs/nemo/NemoApplication.java
@@ -0,0 +1,13 @@
+package com.scs.nemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/scs/nemo/NemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scs.nemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
