bbd416c5ea5b5f40573eb3fde45426347e19616b
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-06-14 22:03:21 +0100

    Add checkout page

---
+++ frontend/src/pages/Checkout.jsx
@@ -0,0 +1,284 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const products = [
+  {
+    id: 1,
+    name: "Throwback Hip Bag",
+    href: "#",
+    color: "Salmon",
+    price: "$90.00",
+    quantity: 2,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
+    imageAlt:
+      "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
+  },
+  {
+    id: 2,
+    name: "Medium Stuff Satchel",
+    href: "#",
+    color: "Blue",
+    price: "$32.00",
+    quantity: 1,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+    imageAlt:
+      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
+  },
+  // More products...
+];
+
+export default function Checkout() {
+  return (
+    <>
+      <Header />
+      <div className="bg-gray-100 p-12">
+        <div className="flex flex-row justify-center">
+          <div className="md:grid md:grid-cols-2 md:gap-6 w-full flex-1 mr-10">
+            <div className="mt-5 md:mt-0 md:col-span-2">
+              <div className="px-4 pb-5 sm:p-6">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  Shipping Information
+                </h3>
+              </div>
+              <div>
+                <form action="#" method="POST">
+                  <div className="shadow overflow-hidden sm:rounded-md">
+                    <div className="px-4 py-5 bg-white sm:p-6">
+                      <div className="grid grid-cols-6 gap-6">
+                        <div className="col-span-6 sm:col-span-3">
+                          <label
+                            htmlFor="first-name"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            First name
+                          </label>
+                          <input
+                            type="text"
+                            name="first-name"
+                            id="first-name"
+                            autoComplete="given-name"
+                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3">
+                          <label
+                            htmlFor="last-name"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Last name
+                          </label>
+                          <input
+                            type="text"
+                            name="last-name"
+                            id="last-name"
+                            autoComplete="family-name"
+                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-4">
+                          <label
+                            htmlFor="email-address"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Email address
+                          </label>
+                          <input
+                            type="text"
+                            name="email-address"
+                            id="email-address"
+                            autoComplete="email"
+                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3">
+                          <label
+                            htmlFor="country"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Country
+                          </label>
+                          <select
+                            id="country"
+                            name="country"
+                            autoComplete="country-name"
+                            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          >
+                            <option>United States</option>
+                            <option>Canada</option>
+                            <option>Mexico</option>
+                          </select>
+                        </div>
+
+                        <div className="col-span-6">
+                          <label
+                            htmlFor="street-address"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Street address
+                          </label>
+                          <input
+                            type="text"
+                            name="street-address"
+                            id="street-address"
+                            autoComplete="street-address"
+                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                          <label
+                            htmlFor="city"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            City
+                          </label>
+                          <input
+                            type="text"
+                            name="city"
+                            id="city"
+                            autoComplete="address-level2"
+                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                          <label
+                            htmlFor="region"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            State / Province
+                          </label>
+                          <input
+                            type="text"
+                            name="region"
+                            id="region"
+                            autoComplete="address-level1"
+                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                          <label
+                            htmlFor="postal-code"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            ZIP / Postal code
+                          </label>
+                          <input
+                            type="text"
+                            name="postal-code"
+                            id="postal-code"
+                            autoComplete="postal-code"
+                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          {/** cart */}
+          <div className="mt-10 sm:mt-0 flex-1">
+            <div className="px-4 py-5 sm:p-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Order Summary
+              </h3>
+            </div>
+            <div className="bg-white overflow-hidden shadow-lg rounded-lg px-10">
+              <div className="mt-8">
+                <div className="flow-root">
+                  <ul className="-my-6 divide-y divide-gray-200">
+                    {products.map((product) => (
+                      <li key={product.id} className="flex py-6 mb-8">
+                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                          <img
+                            src={product.imageSrc}
+                            alt={product.imageAlt}
+                            className="h-full w-full object-cover object-center"
+                          />
+                        </div>
+
+                        <div className="ml-4 flex flex-1 flex-col">
+                          <div>
+                            <div className="flex justify-between text-base font-medium text-gray-900">
+                              <h3>
+                                <a href={product.href}> {product.name} </a>
+                              </h3>
+                              <p className="ml-4">{product.price}</p>
+                            </div>
+                            <p className="mt-1 text-sm text-gray-500">
+                              {product.color}
+                            </p>
+                          </div>
+                          <div className="flex flex-1 items-end justify-between text-sm">
+                            <select
+                              id="quantity"
+                              name="quantity"
+                              autoComplete="quantity"
+                              className="mt-1 block w-24 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                              selected={product.quantity}
+                            >
+                              <option name="1">1</option>
+                              <option name="2">2</option>
+                              <option name="3">3</option>
+                            </select>
+
+                            <div className="flex">
+                              <button
+                                type="button"
+                                className="font-medium text-indigo-600 hover:text-indigo-500"
+                              >
+                                Remove
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div className="border-t border-gray-200 py-10 px-4 sm:px-6">
+                <div className="flex justify-between text-base font-medium text-gray-700 py-5">
+                  <p>Subtotal</p>
+                  <p>$262.00</p>
+                </div>
+                <div className="flex justify-between text-base font-medium text-gray-700 py-5">
+                  <p>Shipping</p>
+                  <p>$5.00</p>
+                </div>
+                <div className="flex justify-between text-base font-medium text-gray-700 py-5">
+                  <p>Taxes</p>
+                  <p>$0.00</p>
+                </div>
+                <div className="border-t border-gray-200 py-8">
+                  <div className="flex justify-between text-lg font-bold text-gray-900">
+                    <p>Total</p>
+                    <p>$267.00</p>
+                  </div>
+                </div>
+
+                <div className="mt-6">
+                  <a
+                    href="https://tailwindui.com/ecommerce/shopping-cart-page.html#"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                  >
+                    Confirm order
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
