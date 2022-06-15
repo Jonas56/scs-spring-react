9f06c3e3918c519f933c46e5e60aef4890b8ef48
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-06-15 21:16:07 +0100

    Update userRepository

--- backend/src/main/java/com/scs/nemo/user/UserRepository.java
+++ backend/src/main/java/com/scs/nemo/user/UserRepository.java
@@ -2,7 +2,7 @@ package com.scs.nemo.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsernameOrEmail(String username, String email);
 
     User findByUsernameOrEmail(String username, String email);
