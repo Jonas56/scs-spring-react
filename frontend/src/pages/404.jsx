554e5d172b1fcb9f6b17315f9bc5b3dc98f75e6a
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-06-20 18:19:27 +0100

    Fix link

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-6xl font-bold text-center">404</h1>
       <h2 className="text-4xl font-bold text-center">Page not found</h2>
-      <a className="text-indigo-700 p-2 font-bold text-base" href="#">
+      <Link to="/" className="text-indigo-700 p-2 font-bold text-base" href="#">
         Go Back Home &rarr;
-      </a>
+      </Link>
     </div>
   );
 }
