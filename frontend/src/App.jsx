18d9e88fa5f22de5ecf552b18f688cda5efefd21
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-07-06 19:41:23 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,11 @@ import expired from "./api/config";
 const App = () => {
   const dispatch = useDispatch();
 
-<<<<<<< HEAD
   useEffect(() => {
     if (expired) {
       dispatch(logout());
     }
   }, [dispatch]);
-=======
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
->>>>>>> 3d674b8 (Website Content added #42)
 
   return <AppRouter />;
 };
